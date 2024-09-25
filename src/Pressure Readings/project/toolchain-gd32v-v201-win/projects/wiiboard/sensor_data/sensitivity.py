60e15b0964012351acf4b89925f8c47bde96ed36
Wawandu29 <102734256+Wawandu29@users.noreply.github.com>
2024-09-25 19:04:42 +0200

    calibration data process

---
+++ src/Pressure Readings/project/toolchain-gd32v-v201-win/projects/wiiboard/sensor_data/sensitivity.py
@@ -0,0 +1,11 @@
+"""
+This programm calculates the sensitivity of a couple (sample rate, gain)
+
+For that, we need to open a json file
+we need to plot the recordings, select the range of samples we're interested in 
+calculating the average unspecified value for a given weight
+This unspecified value has to been stored in a json file with other values for other weights
+By plotting we may observe a linear regression or not
+If it is the case, we can plot the correlation number
+Otherwise, we can find a more complex polynomial or try another couple (sample rate, gain)
+"""
